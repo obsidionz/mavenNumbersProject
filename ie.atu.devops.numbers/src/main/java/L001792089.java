55e54fe47869a27f3fa7ce47807507179daa395b
L00179208 <L00179208@atu.ie>
2023-10-12 14:37:09 +0100

    Comment added to file and new file created

---
+++ ie.atu.devops.numbers/src/main/java/L001792089.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class L001792089 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the starting number: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the ending number: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Even numbers in the range " + start + " to " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("\nOdd numbers in the range " + start + " to " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        scanner.close();
+    }
+
+}
--- ie.atu.devops.numbers/src/main/java/Numbers.java
+++ ie.atu.devops.numbers/src/main/java/Numbers.java
@@ -1,24 +1,24 @@
-
+// This is a comment from Aneesh LO0179208
 public class Numbers {
 
-	public static void sum(){
-		// TODO 
-	}
-	
-	public static void subtract(){
-	   // TODO 
-	}
-	
-	public static void largest(){
-		// TODO 
+	public static void sum() {
+		// TODO
 	}
 
-	public static void smallest(){
-		// TODO 
+	public static void subtract() {
+		// TODO
 	}
-	
+
+	public static void largest() {
+		// TODO
+	}
+
+	public static void smallest() {
+		// TODO
+	}
+
 	public static void main(String[] args) {
-		// TODO 
+		// TODO
 
 	}
 
